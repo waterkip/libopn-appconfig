7d0b1e161ef1879561208b53b6477f86fd50a47a
Wesley Schwengle <wesley@schwengle.net>
2015-07-04 12:18:06 +0200

    Initial commit of testsuite.

---
+++ lib/OPN/AppConfig.pm
@@ -0,0 +1,25 @@
+package Foo;
+use Moose;
+
+
+
+
+__PACKAGE__->meta->make_immutable;
+
+__END__
+
+=head1 NAME
+
+OPN::AppConfig - A AppConfig package
+
+=head1 DESCRIPTION
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+Wesley Schwengle
+
+=head1 LICENSE and COPYRIGHT
+
+Wesley Schwengle, 2015.
